ec74ce9e6469cb4546e4dd9766453dc29d60d27b
Tanveer Najib <tanveernajib@gmail.com>
2023-10-21 18:11:58 +0100

    Show year in title for movies

--- resources/lib/tmdb.py
+++ resources/lib/tmdb.py
@@ -7,6 +7,14 @@ BASE_URL = "https://api.themoviedb.org/3/"
 BASE_POSTER_PATH = "https://www.themoviedb.org/t/p/w600_and_h900_bestv2"
 BASE_BACKDROP_PATH = "https://www.themoviedb.org/t/p/original"
 
+def _get_year(datestr):
+    if not datestr:
+        return "-"
+    parts = datestr.split("-")
+    if len(parts) > 0:
+        return parts[0]
+    return "-"
+
 def search_movie(query, key, page = 1):
     """
     Searches the TMDb API for movies
@@ -22,7 +30,7 @@ def search_movie(query, key, page = 1):
     movies = []
     for result in json["results"]:
         tmdb_id = result["id"]
-        title = result["title"]
+        title = result["title"] + " (" + _get_year(result["release_date"]) + ")"
         plot = result["overview"]
         poster = BASE_POSTER_PATH + result["poster_path"]
         fanart = BASE_BACKDROP_PATH + result["backdrop_path"] if result["backdrop_path"] else None
